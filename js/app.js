d7382fe87eb00ceb2cdbf4352e09a1cb32a1fd24
Paul Merizalde <merizaldepaul1999>
2023-06-08 19:42:48 -0500

    taller 14: directivas

--- js/app.js
+++ js/app.js
@@ -1,12 +1,32 @@
-console.log("Componente Vue")
-console.log(Vue)
+console.log("Componente Vue");
+console.log(Vue);
 
 const app = Vue.createApp({
-    //se configuran opciones (Options API)
-    template: `
-    <h1>HOLA MUNDO</h1>
-    <p>Desde Vue.JS</p>
-    `
-})
+  //se configuran opciones (Options API)
+  // template: `
+  // <h1>HOLA MUNDO</h1>
+  // <p>Desde Vue.JS</p>
+  // <p>{{1+1}}</p>
+  // `
+  //opcion de Option API que me permite declarar propiedades reactivas
+  //vincular informacion de la vista con mi programacion JavaScript
+  data() {
+    return {
+      nombre: "Paul",
+      apellido: "Merizalde",
+      mensaje: "Hola mundo 2 desde Vue.JS",
+    };
+  },
+  methods: {
+    cambiarNombre() {
+      console.log("cambiar nombre");
+      this.nombre = "Leonardo";
+    },
+    cambiarApellido() {
+      console.log("cambiar apellido");
+      this.apellido = "Proaño";
+    },
+  },
+});
 
-app.mount('#app')
+app.mount("#app");
