3a6cb9933dcc98065482abf2e2d269540c3ef553
Paul Merizalde <merizaldepaul1999>
2023-06-21 12:14:26 -0500

    Tarea 8

--- js/calculadoraWeb.js
+++ js/calculadoraWeb.js
@@ -3,8 +3,10 @@ const app = Vue.createApp({
     return {
       labelResultado: "",
       operacion: "",
-      num1: "",
-      num2: "",
+      numText1: "",
+      numText2: "",
+      num1: 0,
+      num2: 0,
       decimal: 0,
       bol: false,
     };
@@ -19,16 +21,32 @@ const app = Vue.createApp({
         this.bol = false;
       }
       if (this.decimal > 1 && valor === ".") valor = "";
-      this.operacion === "" ? (this.num1 += valor) : (this.num2 += valor);
-      this.labelResultado = parseFloat(this.operacion == "" ? this.num1 : this.num2);
+      this.operacion === ""
+        ? (this.numText1 += valor)
+        : (this.numText2 += valor);
+      this.labelResultado = this.operacion == "" ? this.numText1 : this.numText2
+
     },
     igual() {
-      if(this.num2 !=""){
+      if (this.numText2 != "") {
         this.bol = true;
-        this.num1 = parseFloat(this.num1);
-        this.num2 = parseFloat(this.num2);
-        console.log(this.resultado)
-        this.labelResultado = this.resultado;
+        this.num1 = parseFloat(this.numText1);
+        this.num2 = parseFloat(this.numText2);
+        this.labelResultado = this.resultado();
+      }
+    },
+    resultado() {
+      switch (this.operacion) {
+        case "/":
+          return this.dividir;
+        case "x":
+          return this.multiplicar;
+        case "-":
+          return this.restar;
+        case "+":
+          return this.sumar;
+        default:
+          return 0;
       }
     },
     guardarOperacion(op) {
@@ -36,26 +54,26 @@ const app = Vue.createApp({
     },
     reset() {
       this.operacion = "";
-      this.num1 = "";
-      this.num2 = "";
+      this.numText1 = "";
+      this.numText2 = "";
     },
   },
   computed: {
-    resultado() {
-      const num1 = parseFloat(this.num1);
-      const num2 = parseFloat(this.num2);
-      switch (this.operacion) {
-        case "/":
-          return num1 / num2;
-        case "x":
-          return num1 * num2;
-        case "-":
-          return num1 - num2;
-        case "+":
-          return num1 + num2;
-        default:
-          return 0;
-      }
+    dividir() {
+      console.log("entro al computed dividir: ");
+      return this.num1 / this.num2;
+    },
+    multiplicar() {
+      console.log("entro al computed multiplicar: ");
+      return this.num1 * this.num2;
+    },
+    restar() {
+      console.log("entro al computed restar: ");
+      return this.num1 - this.num2;
+    },
+    sumar() {
+      console.log("entro al computed sumar: ");
+      return this.num1 + this.num2;
     },
   },
 });
